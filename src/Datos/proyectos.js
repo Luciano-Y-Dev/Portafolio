d727971e98fc0d802ce92e25c9479bdcf4d07385
Luciano Yanes <lucianoyanesmart3@gmail.com>
2026-05-05 18:17:44 -0400

    Cambie las fotos de los proyectos por webp para eficiencia

--- src/Datos/proyectos.js
+++ src/Datos/proyectos.js
@@ -5,7 +5,7 @@ export const projects = [
       "Plataforma institucional de alto rendimiento para el sector agrícola. Optimizada para carga rápida mediante arquitectura estática, hecha en astro ya que la empresa pidio una pagina rapida.",
     tags: ["Astro 6", "TailwindCSS 4", "TypeScript", "Vercel"],
     link: "https://github.com/o-Lux-o/Agroturen",
-    image: "/ProjectImg/Agroturen-pImage.png",
+    image: "/ProjectImg/Agroturen-pImage.webp",
     featured: true,
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
       "Aplicación interactiva que utiliza la API de la NASA para descubrir la imagen astronómica capturada el día de tu nacimiento con traducción automática.",
     tags: ["Python", "Reflex", "NASA API", "Tailwind CSS"],
     link: "https://github.com/o-Lux-o/Nasa-Project",
-    image: "public/ProjectImg/Nasa-pImage.png",
+    image: "public/ProjectImg/Nasa-pImage.webp",
     featured: false,
   },
   {
@@ -23,7 +23,7 @@ export const projects = [
       "Proyecto de Data Science enfocado en predecir la fuga de clientes bancarios mediante análisis estadístico y modelos de Machine Learning.",
     tags: ["Python", "Scikit-learn", "Pandas", "Streamlit"],
     link: "https://github.com/o-Lux-o/BankChurn",
-    image: "public/ProjectImg/BankChurn-Image.png",
+    image: "public/ProjectImg/BankChurn-Image.webp",
     featured: true,
   },
   {
@@ -32,7 +32,7 @@ export const projects = [
       "Pipeline de análisis de datos para evaluar el ROI y la madurez de adopción de Inteligencia Artificial en las empresas más grandes del mundo.",
     tags: ["Python", "Pandas", "Matplotlib", "Seaborn"],
     link: "https://github.com/o-Lux-o/Fortune500-AI-Analysis-Pipeline",
-    image: "public/ProjectImg/AI-pImage.png",
+    image: "public/ProjectImg/AI-pImage.webp",
     featured: false,
   },
 ];
